2ea8f7741f19b4ace3004480ac3f1889bb5c763e
HideonBush <2519469152@qq.com>
2024-11-11 14:53:37 +0800

    commit all

---
+++ src/test/java/L2022211999_15_Test.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 将输入(v1,v2)划分为:
+ *  v1 > v2:
+ *      len(v2) < len(v1)
+ *      len(v2) >=len(v1)
+ *  v1 < v2:
+ *      len(v1) < len(v2)
+ *      len(v1) >=len(v2)
+ *  others:
+ *      v1=v2
+ *  以上各划分均又分为有无前导零
+ */
+public class L2022211999_15_Test {
+    Solution s = new Solution();
+
+    /**
+     * 测试版本号相同
+     * 测试用例:
+     *      (1.001.011, 1.1.11),
+     *      (1.2.3,1.2.3)
+     * 期望输出：
+     *      0,0
+     */
+    @Test
+    public void test_equal() {
+        assertEquals(0,s.compareVersion("1.001.011", "1.1.11"));
+        assertEquals(0,s.compareVersion("1.2.3", "1.2.3"));
+    }
+
+    /**
+     * 测试v1>v2
+     * 测试用例
+     *      (1.2.3, 1.2.2),
+     *      (1.2.3, 1.2),
+     *      (1.3, 1.2.3),
+     *      (1.02.003.4 1.2.3),
+     * 期望输出
+     *      1,1,1,1
+     */
+    @Test
+    public void test_v1_larger() {
+        assertEquals(1,s.compareVersion("1.2.3", "1.2.2"));
+        assertEquals(1,s.compareVersion("1.2.3", "1.2"));
+        assertEquals(1,s.compareVersion("1.3", "1.2.3"));
+        assertEquals(1,s.compareVersion("1.02.003.4", "1.2.3"));
+    }
+    /**
+     * 测试v1< v2
+     * 测试用例
+     *      (1.2.2, 1.2.3),
+     *      (1.2, 1.2.3),
+     *      (1.2.3, 1.3),
+     *      (1.2.3, 1.02.003.4),
+     * 期望输出
+     *      -1,-1,-1,-1
+     */
+    @Test
+    public void test_v1_smaller() {
+        assertEquals(-1, s.compareVersion("1.2.2", "1.2.3"));
+        assertEquals(-1, s.compareVersion("1.2", "1.2.3"));
+        assertEquals(-1, s.compareVersion("1.2.3", "1.3"));
+        assertEquals(-1, s.compareVersion("1.2.3", "1.02.003.4"));
+    }
+}
